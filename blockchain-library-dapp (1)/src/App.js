fba0323cb59ecbc4b7090ded1f104f76b6e74153
BlockchainLibrary-BL <hello@blockchain-library.org>
2025-06-04 13:51:54 +0100

    Initial commit for Genesis Badge DApp
    Uploaded React project files for the Blockchain Library Genesis Badge minting DApp with WalletConnect integration and donation support. Ready for Netlify deployment.

---
+++ blockchain-library-dapp (1)/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Genesis Badge</h1>
+      <p>This is the Blockchain Library DApp</p>
+    </div>
+  );
+}
+
+export default App;
